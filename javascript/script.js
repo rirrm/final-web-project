56ee1b4ba490cdadaa41360545f94c20287116ee
rirrm <ritamalaa.1@gmail.com>
2022-12-21 19:35:57 +0100

    changes

--- javascript/script.js
+++ javascript/script.js
@@ -11,3 +11,25 @@ function validoInput(){
         alert("Ke shenuar te dhenat me sukses <3");
     }
 }
+
+var i = 0;
+// var imgArray = [
+// "./images/slider1.jpg",
+// "./images/slider2.jpg",
+// "./images/slider3.jpg"
+// ];
+
+var imgArray = [
+    
+]
+
+function ndrroImg() {
+    document.getElementById('slideshow').src = imgArray[i];
+if (i < imgArray.length - 1 ) {
+    i++;
+}else {
+    i=0;
+}
+setTimeout("ndrroImg()", 1500)
+}
+document.body.addEventListener('load', ndrroImg());

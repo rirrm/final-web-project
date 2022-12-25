6edcd0b28baab83cba7de2e72d215943fe3ae986
rirrm <ritamalaa.1@gmail.com>
2022-12-25 17:31:04 +0100

    ndryshime

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,3 @@
-function validoInput(){
-
-    var emailiUserit=document.getElementById('email').value;
-    var passiUserit=document.getElementById('password').value;
-
-    if(emailiUserit==""){
-        alert("Duhet te shenosh email!")
-    }else if(passiUserit==""){
-        alert("Duhet te shenosh password!")
-    }else{
-        alert("Ke shenuar te dhenat me sukses <3");
-    }
-}
-
 var i = 0;
  var imgArray = [
  "./images/sliderr.jpg",

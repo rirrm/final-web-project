a326e9c99fd4b100c14a697cd69973931c7c86e0
kleazz <kk56157@ubt-uni.net>
2023-02-17 15:32:12 +0100

    changes

--- classes/signup-contr.classes.php
+++ classes/signup-contr.classes.php
@@ -13,9 +13,51 @@ class SignUpContr {
 
     private function emptyInput(){
         $result;
-        if( ){
-
-        }
+        if(empty($this->$emailaddress)||empty($this->$username)||empty($this->$password)){
+            $result=false;
+         }
+         else{
+            $result=true;
+         }
+         return $result;
     }
+
+    private function invalidUsername(){
+        $result;
+        if(!preg_match("/^[a-zA-Z0-9]*$/", $this->$username)){
+            $result=false;
+        }
+        else{
+           $result=true;
+        }
+        return $result;
+        }
+
+    private function invalidEmail(){
+        $result;
+        if(!filter_var($this->$email, FILTER_VALIDATE_EMAIL)){
+            $result=false;
+        }
+        else{
+           $result=true;
+        }
+        return $result;
+        }
+
+    private function invalidPassword(){
+        $result;
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $number    = preg_match('@[0-9]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+        
+        if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($password) < 8) {
+            $result=false;
+        }else{
+            $result=true;
+        }
+        return $result;
+    }
+    }
+
  
-}

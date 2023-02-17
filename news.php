c8adf50314a11c0a080ac4f350b311b38099239e
rirrm <ritamalaa.1@gmail.com>
2023-02-17 15:32:14 +0100

    includes header and footer

---
+++ news.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/news.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+include "header.php";  
+?>
+    <main>
+       
+        <div id="lajmet">
+            <div id="lajmibox">
+    <div class="lajmi"><div class="ph"><img src=""></div></div>
+    <div class="lajmi"><div class="ph"><img src=""></div></div>
+    <div class="lajmi"><div class="ph"><img src=""></div></div>
+            </div>
+    </div>
+    <div id="lajmet">
+        <div id="lajmibox">
+<div class="lajmi"><div class="ph2"><img src=""></div></div>
+<div class="lajmi"><div class="ph2"><img src=""></div></div>
+        </div>
+</div>
+<div id="lajmet">
+        <div id="lajmibox">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo architecto excepturi necessitatibus fuga accusamus facilis inventore velit eum obcaecati! Culpa aut perspiciatis tempora mollitia nesciunt voluptatibus fuga explicasbo ullam deserunt?</p>
+    <form action="">
+        
+    </form>
+        </div>
+</div>
+    </main>
+   <?php 
+    include "footer.php";  
+?>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+</head>
+
+<?php 
+include "header.php";  
+?>
+    <body>
+        <div class="kufiza">
+            <div>
+              <img src="./images/haileybw.jpg" alt="" class="lsimg">
+          </div>
+          <div class="center">
+            <h1>Create Account</h1>
+            <form class="login_form" action="./signup.html" method="post" name="form" onsubmit="return validated()">
+                <div class="txt_field">
+                    <input autocomplete="off" type="text" name="emailaddress" id="email" required>
+                    <span></span>
+                    <label for="emailaddress">Email</label>
+                </div>
+              <div class="txt_field">
+                <input autocomplete="off" type="text" name="username" id="user" required>
+                <span></span>
+                <label for="username">Username</label>
+              </div>
+              <div class="txt_field">
+                <input type="password" name="password" id="pass" required>
+                <span></span>
+                <label for="password" >Password</label>
+              </div>
+              <input type="submit" name="login-btn" value="REGISTER">
+              <div class="signup_link">
+                Already have an account?<a href="login.html">Sign in!</a>
+              </div>
+            </form>
+          </div>
+          
+          </div>
+       <?php 
+    include "footer.php";  
+?>
+    <script src="javascript/validationsignup.js"></script>
+</body>
+</html>

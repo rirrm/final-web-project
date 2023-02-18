757334d553dad95e7fae0307a87bbfc9a7600d53
rirrm <ritamalaa.1@gmail.com>
2023-02-18 19:13:12 +0100

    ..

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ class Database{
     public function __construct(){
         try{
             session_start();
-            $link=new PDO('mysql:host=localhost;dbname=lajmet','root','');
+            $link=new PDO('mysql:host=localhost;dbname=lajmet','RitaM','Ritarita123');
             $this->pdo=$link;
         }catch(PDOException $exception){
             die($exception->getMessage());

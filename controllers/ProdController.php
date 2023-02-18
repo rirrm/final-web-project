ad1ee20593cceef0210c18265cd5f28dcf41c445
rirrm <ritamalaa.1@gmail.com>
2023-02-18 19:12:59 +0100

    ...

---
+++ controllers/ProdController.php
@@ -0,0 +1,61 @@
+<?php
+    require_once './config/database.php';
+
+    class ProdController{
+        public $db;
+
+        public function __construct(){
+            $this->db=new Database;
+        }
+
+        public function readData(){
+            $query = $this->db->pdo->query('SELECT * from produkti');
+
+            return $query->fetchAll();
+        }
+
+        public function insert($request){
+            $request['foto']='./images/'.$request['foto'];
+            $query=$this->db->pdo->prepare('INSERT INTO lajmi (prod_pershkrimi,prod_foto,prod_cmimi)
+            VALUES(:prod_pershkrimi,:prod_foto,:prod_cmimi)');
+
+            $query->bindParam(':prod_pershkrimi',$request['pershkrimi']);
+            $query->bindParam(':prod_foto',$request['foto']);
+            $query->bindParam(':prod_cmimi',$request['cmimi']);
+            $query->execute();
+
+            return header('Location: dashboard.php');
+        }
+
+        public function edit($id){
+            $query =$this->db->pdo->prepare('SELECT * FROM produkti WHERE id = :id');
+            $query->bindParam(':id',$id);
+            $query->execute();
+
+            return $query->fetch();
+        }
+
+        public function update($request,$id){
+            $request['foto']='./images/'.$request['foto'];
+            $query=$this->db->pdo->prepare('UPDATE produkti SET prod_pershkrimi = :prod_pershkrimi,
+            prod_foto = :prod_foto, prod_cmimi=:prod_cmimi
+            WHERE id =:id');
+            $query->bindParam(':prod_pershkrimi',$request['pershkrimi']);
+            $query->bindParam(':prod_foto',$request['foto']);
+            $query->bindParam(':prod_cmimi',$request['cmimi']);
+            $query->bindParam(':id',$id);
+            $query->execute();
+
+            return header('Location: dashboard.php');
+        }
+
+        public function delete($id){
+            $query =$this->db->pdo->prepare('DELETE FROM produkti WHERE id=:id');
+            $query->bindParam(':id',$id);
+            $query->execute();
+
+            return header('Location: dashboard.php');
+        }
+
+    }
+?>

f362940ac36aa9793c8b0b5e08442a47c09d391f
kleazz <kk56157@ubt-uni.net>
2023-02-23 17:12:32 +0100

    ndryshime

--- login.php
+++ login.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true){
+    header("location: index.php");
+    exit;
+}
+
 if(isset($_GET['error'])){
   $error = $_GET['error'];
   echo '<script>alert("'.$error.'")</script>';

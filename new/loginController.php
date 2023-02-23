86d001fe222006688270f816b5a1434958f4d5b5
kleazz <kk56157@ubt-uni.net>
2023-02-23 14:58:22 +0100

    ndryshim

--- new/loginController.php
+++ new/loginController.php
@@ -2,45 +2,74 @@
 include_once 'adminClass.php';
 include_once 'simpleUserClass.php';
 require_once 'userMapper.php';
-
 if (isset($_POST['login-btn'])) {
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        if(usernameAndPasswordCorrect($username,$password)){
-            return header("Location:../index.php");
+    $login=new LoginLogic($_POST);
+    $EmptyFields=$login->emptyFields();
+    $usernameExists=$login->usernameExists();
+    $passwordVerify=$login->passwordVerify();
+
+    if($usernameExists){
+            if($passwordVerify){
+                $message = "Jeni bere log in me sukses!";
+                header("Location:../login.php?error=".urlencode($message));
+           }else{
+                $message = "Password Gabim!";
+                header("Location:../login.php?error=".urlencode($message));
+            }
         }
         else{
-            return header("Location:../login.php");
+            $message = "Username nuk ekziston!";
+            header("Location:../login.php?error=".urlencode($message));
         }
-    } else {
-        // handle the case where the keys are not set
     }
-}
 
-function usernameAndPasswordCorrect($username, $password)
-{
-    $mapper = new UserMapper();
-    $user = $mapper->getUserByUsername($username);
-    if ($user == null || count($user) == 0) {
+class LoginLogic{
+
+    private $username="";
+    private $password="";
+
+    public function __construct($formData)
+    {
+        $this->username=$formData['username'];
+        $this->password=$formData['password'];
+    }
+
+    function emptyFields(){
+        if(empty($this->username)|| empty($this->password)){
+            return true;
+        }
         return false;
     }
-    else if (password_verify($password, $user['userpassword'])) {
-        
-        if ($user['role'] == 1) {
-            $obj = new Admin($user['userID'], $user['username'], $user['userpassword'], $user['role']);
-            $obj->setSession();
-        } else {
-            $obj = new SimpleUser($user['userID'], $user['username'], $user['userpassword'], $user['role']);
-            $obj->setSession();
+
+    function usernameExists(){
+        $mapper = new UserMapper();
+        $user = $mapper->getUserByUsername($this->username);
+        if ($user == null || count($user) == 0) {
+            return false;
+        }else{
+            return true;
         }
-        $_SESSION['loggedin'] = true;
-        return true;
-        
-    } else return false;
+    }
+    function passwordVerify()
+    {
+        $mapper = new UserMapper();
+        $user = $mapper->getUserByUsername($this->username);
+        if (password_verify($password, $user['userpassword'])) {
+            
+            if ($user['role'] == 1) {
+                $obj = new Admin($user['userID'], $user['username'], $user['userpassword'], $user['role']);
+                $obj->setSession();
+            } else {
+                $obj = new SimpleUser($user['userID'], $user['username'], $user['userpassword'], $user['role']);
+                $obj->setSession();
+            }
+            $_SESSION['loggedin'] = true;
+            return true;
+            
+        } else return false;
+    }
+
+    
 }
 
 
-
-    
-?>

7a27c1acef34ee1d1dfa238dcf10a4ac130aefd0
rirrm <ritamalaa.1@gmail.com>
2023-02-24 19:46:42 +0100

    ....

--- header.php
+++ header.php
@@ -5,7 +5,18 @@ if (session_status() == PHP_SESSION_NONE) {
   session_start();
 }
 ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
 
